cdef74ecd8cd4c7b756a6919ac3843fad8aa7911
Toprak Ege <104670963+DevAlexiss@users.noreply.github.com>
2022-05-28 17:08:07 +0300

    register.php

---
+++ frontend/register.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<link rel='stylesheet' href='main.css'>
+<title>Habbo - Play Now</title>
+</head>
+<body>
+<div class='alexissmodal'>
+<div class='logo'><img src='https://habbofont.net/font/habbo_new_big/habbo.gif'></div>
+<div class='rightlogin'>
+<h1>Register Page</h1>
+<br>
+<input placeholder="Username"><img class='loginico' src='https://habborator.org/archive/icons/mini/new_19.gif'></input><br><br>
+<input placeholder="Password"><img class='loginico' src='https://habborator.org/archive/icons/mini/locked_small.gif'></input><br><br>
+<input placeholder="Repeat Password"><img class='loginico' src='https://habborator.org/archive/icons/mini/locked_small.gif'></input><br><br>
+<div style="float: right;width: 66%;"><button class='btnregister'>Register</button></div>
+</div>
+</div>
+<div class='leftbg'></div>
+</body>
+</html>
