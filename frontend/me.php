fe354314a914166c9274a64e83159f0da893a1c0
Toprak Ege <104670963+DevAlexiss@users.noreply.github.com>
2022-05-29 09:09:31 +0300

    Add files via upload

---
+++ frontend/me.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<link rel='stylesheet' href='main.css'>
+<title>Habbo - Play Now</title>
+</head>
+<body>
+<div class='alexissmodal'>
+<div class='logo'><img src='https://habbofont.net/font/habbo_new_big/habbo.gif'><br><div class='walletbar'><img src='https://www.leethotel.biz/assets/images/icons/credits.png'>100</div><div class='walletbar'><img src='https://www.leethotel.biz/assets/images/icons/credits.png'>100</div><div class='walletbar'><img src='https://www.leethotel.biz/assets/images/icons/credits.png'>100</div></div>
+<div class='rightlogin'>
+<button>Flash</button>
+<button style='right:0;position:absolute;'>Beta</button>
+</div>
+</div>
+<div class='leftbg'></div>
+</body>
+</html>
